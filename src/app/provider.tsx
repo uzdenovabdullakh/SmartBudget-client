b11c1aeeb5a6052137136917a77c838c1f1dbd87
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2024-12-19 16:20:58 +0300

    style: [KB-23] add reset css to chakra provider

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -2,7 +2,11 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
 
 function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider theme={theme} resetCSS>
+      {children}
+    </ChakraProvider>
+  );
 }
 
 export default Providers;
