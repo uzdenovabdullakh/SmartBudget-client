dea432b7b9a24c5b8b931d85fd4b1eb6aa7998c6
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-01-28 11:10:05 +0300

    feat: [KB-85] add navigation when clicking on an account from sidebar

--- src/components/account/AccountsTable.tsx
+++ src/components/account/AccountsTable.tsx
@@ -1,29 +1,46 @@
 import { Account } from "@/lib/types/account.types";
 import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { useParams, useRouter } from "next/navigation";
 
 type AccountTableProps = {
   accounts: Account[];
 };
 
-export const AccountsTable = ({ accounts }: AccountTableProps) => (
-  <Table variant="simple">
-    <Thead>
-      <Tr>
-        <Th>Account</Th>
-        <Th>Date</Th>
-        <Th>Type</Th>
-        <Th>Amount</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      {accounts.map((account) => (
-        <Tr key={account.id}>
-          <Td>{account.name}</Td>
-          <Td>{new Date(account.createdAt).toLocaleDateString()}</Td>
-          <Td>{account.type}</Td>
-          <Td>{account.amount}</Td>
+export const AccountsTable = ({ accounts }: AccountTableProps) => {
+  const router = useRouter();
+  const params = useParams();
+  const budgetId = Array.isArray(params?.id) ? params.id[0] : params?.id;
+
+  const handleAccountClick = (accountId: string) => {
+    if (budgetId) {
+      router.push(`/dashboard/${budgetId}/account/${accountId}`);
+    }
+  };
+
+  return (
+    <Table variant="simple">
+      <Thead>
+        <Tr>
+          <Th>Account</Th>
+          <Th>Date</Th>
+          <Th>Type</Th>
+          <Th>Amount</Th>
         </Tr>
-      ))}
-    </Tbody>
-  </Table>
-);
+      </Thead>
+      <Tbody>
+        {accounts.map((account) => (
+          <Tr
+            key={account.id}
+            onClick={() => handleAccountClick(account.id)}
+            cursor="pointer"
+          >
+            <Td>{account.name}</Td>
+            <Td>{new Date(account.createdAt).toLocaleDateString()}</Td>
+            <Td>{account.type}</Td>
+            <Td>{account.amount}</Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
+};
