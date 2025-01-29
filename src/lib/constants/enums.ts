85de3d57e28126a1ad374c4e0f91d9f8e926f5e7
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-01-29 14:02:06 +0300

    feat: [KB-92] add budget creating logic in AddBudgetModal

--- src/lib/constants/enums.ts
+++ src/lib/constants/enums.ts
@@ -3,3 +3,14 @@ export enum UnlinkedAccountType {
   CARD = "card",
   SAVINGS = "savings",
 }
+
+export enum BudgetCurrency {
+  USD = "USD",
+  RUB = "RUB",
+  EUR = "EUR",
+}
+
+export enum CurrencyPlacement {
+  BEFORE = "before",
+  AFTER = "after",
+}
