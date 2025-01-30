2f6fc696281b2950a10e6bcc2dc4371df9180208
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-01-30 20:54:27 +0300

    feat: create delete modal

--- src/components/modals/delete/Delete.tsx
+++ src/components/modals/delete/Delete.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 import { DefaultModalProps } from "@/lib/types/types";
+import { Button } from "@chakra-ui/react";
 import { DefaultModal } from "..";
 
-export const DeleteModal = ({ isOpen, onClose }: DefaultModalProps) => {
-  return (
-    <DefaultModal
-      isOpen={isOpen}
-      onClose={onClose}
-      title="Delete"
-      size="md"
-      body={<>Delete</>}
-      footer={<>Delete</>}
-    />
-  );
-};
+type DeleteModalProps = {
+  entity: {
+    type: string;
+    name: string;
+  };
+  onDelete: () => void;
+} & DefaultModalProps;
+
+export const DeleteModal = ({
+  isOpen,
+  onClose,
+  onDelete,
+  entity,
+}: DeleteModalProps) => (
+  <DefaultModal
+    isOpen={isOpen}
+    onClose={onClose}
+    title={`Delete ${entity.type}`}
+    size="sm"
+    body={`Do you want to delete ${entity.name}?`}
+    footer={
+      <Button colorScheme="red" onClick={onDelete}>
+        Confirm Delete
+      </Button>
+    }
+  />
+);
