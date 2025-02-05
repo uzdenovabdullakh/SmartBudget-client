c203a910fade2ee9496a8c55f239114dc8d889d1
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-02-06 00:03:45 +0300

    feat: [KB-98] add next-auth

---
+++ src/lib/types/env.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_API_URL: string;
+    YANDEX_CLIENT_ID: string;
+    YANDEX_CLIENT_SECRET: string;
+    NEXTAUTH_SECRET: string;
+    NEXTAUTH_URL: string;
+  }
+}
