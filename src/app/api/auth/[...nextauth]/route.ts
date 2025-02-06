feb16a3b8a05b623f65cda8da205f2d3599f1744
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-02-06 16:32:50 +0300

    refactor: move authOptions in constants

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,37 +1,5 @@
-/* eslint-disable no-param-reassign */
-import NextAuth, { NextAuthOptions } from "next-auth";
-import YandexProvider from "next-auth/providers/yandex";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    YandexProvider({
-      clientId: process.env.YANDEX_CLIENT_ID!,
-      clientSecret: process.env.YANDEX_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          scope: "login:email login:info",
-        },
-      },
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async jwt({ token, user, account }) {
-      if (account && user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.id as string;
-        session.user.email = token.email;
-        session.user.name = token.name;
-      }
-      return session;
-    },
-  },
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/constants/oauth";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
