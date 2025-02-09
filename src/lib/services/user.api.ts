4703cd565a5b6abcf74fa7eae7f7aae25e72a449
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-02-09 21:40:04 +0300

    refactor: [KB-57] removed unnecessary fields in UserDetails, add tags in user api

--- src/lib/services/user.api.ts
+++ src/lib/services/user.api.ts
@@ -1,18 +1,21 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { axiosBaseQuery } from "../axiosBaseQuery";
 import { UserDetails } from "../types/user.types";
-import { MutationResponse, ResponseWithoutData } from "../types/types";
+import { ResponseWithoutData } from "../types/types";
 import { UpdateUserDto } from "../validation/user.schema";
 
 export const usersApi = createApi({
   reducerPath: "users",
   baseQuery: axiosBaseQuery({ baseUrl: "/users" }),
+  tagTypes: ["User"],
   endpoints: (builder) => ({
     getUser: builder.query<UserDetails, void>({
       query: () => ({ url: "/", method: "GET" }),
+      providesTags: ["User"],
     }),
-    updateUser: builder.mutation<MutationResponse<UserDetails>, UpdateUserDto>({
+    updateUser: builder.mutation<ResponseWithoutData, UpdateUserDto>({
       query: (data: UpdateUserDto) => ({ url: "/", method: "PATCH", data }),
+      invalidatesTags: ["User"],
     }),
     deleteProfile: builder.mutation<ResponseWithoutData, void>({
       query: () => ({ url: "/", method: "DELETE" }),
