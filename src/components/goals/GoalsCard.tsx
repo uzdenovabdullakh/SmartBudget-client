38204b5ae1da8b47aa3de758269c2a97ec82821e
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-03-05 20:36:56 +0300

    feat: [KB-129] create EditGoalModal

--- src/components/goals/GoalsCard.tsx
+++ src/components/goals/GoalsCard.tsx
@@ -1,8 +1,9 @@
-import { Box, Text, HStack } from "@chakra-ui/react";
+import { Box, Text, HStack, useDisclosure } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 import { Goal } from "@/lib/types/goal.types";
 import { BaseCard } from "../ui/BaseCard";
 import { GoalAnimation } from "../ui/Animations";
+import { EditGoalModal } from "../modals/edit-goal/EditGoalModal";
 
 type GoalsCardProps = {
   goal: Goal;
@@ -10,11 +11,12 @@ type GoalsCardProps = {
 
 export function GoalsCard({ goal }: GoalsCardProps) {
   const { t } = useTranslation();
+  const { onOpen, onClose, isOpen } = useDisclosure();
 
   return (
     <BaseCard height="250px" width="250px" position="relative">
       <HStack justifyContent="space-between" alignItems="start">
-        <Box flex={1}>
+        <Box flex={1} onClick={onOpen}>
           <Text fontSize="xl" fontWeight="bold">
             {goal.name}
           </Text>
@@ -23,6 +25,7 @@ export function GoalsCard({ goal }: GoalsCardProps) {
             {t("last_updated")}: {new Date(goal.updatedAt).toLocaleDateString()}
           </Text>
         </Box>
+        <EditGoalModal onClose={onClose} isOpen={isOpen} goal={goal} />
       </HStack>
     </BaseCard>
   );
